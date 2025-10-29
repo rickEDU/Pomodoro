2d1705b605e6407a75bb032bc2871b9757b92de2
Eduardo Santos <eduardo.h3nriqu3@gmail.com>
2025-10-29 18:55:46 -0300

    :hammer: - countdown component

--- src/components/Cycles/Cycles.tsx
+++ src/components/Cycles/Cycles.tsx
@@ -3,7 +3,7 @@ import { CyclesPoint } from '@components/CyclesPoint/CyclesPoint';
 
 export const Cycles = () => {
   const renderPoints = () => {
-    const numberOfPoints = 12;
+    const numberOfPoints = 8;
     const points = Array.from({ length: numberOfPoints });
     return points.map((_, index) => {
       let color = index % 2 === 0 ? 'blue' : 'red';
--- src/components/TimeDisplay/TimeDisplay.tsx
+++ src/components/TimeDisplay/TimeDisplay.tsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from 'react';
 import styles from './TimeDisplay.module.css';
 
-export const TimeDisplay = () => {
-  return <div className={styles.timer}>00:00</div>;
+interface TimeDisplayProps {
+  initialTime: number;
+  isStartCoutdown: boolean;
+  onFinish?: () => void;
+}
+
+export const TimeDisplay = ({
+  initialTime = 120,
+  isStartCoutdown = false,
+  onFinish,
+}: TimeDisplayProps) => {
+  const [timeLeft, setTimeLeft] = useState(initialTime);
+
+  useEffect(() => {
+    if (timeLeft <= 0 || !isStartCoutdown) {
+      if (onFinish) onFinish();
+      return;
+    }
+
+    const interval = setInterval(() => {
+      setTimeLeft((prev: number) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [timeLeft, onFinish, isStartCoutdown]);
+
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+
+  const formattedTime = `${String(minutes).padStart(2, '0')}:${String(
+    seconds,
+  ).padStart(2, '0')}`;
+
+  return <div className={styles.timer}>{formattedTime}</div>;
 };
